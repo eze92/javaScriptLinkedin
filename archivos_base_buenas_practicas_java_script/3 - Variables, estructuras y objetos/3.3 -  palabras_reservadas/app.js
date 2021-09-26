4d24d3ca0eceb1fdbc284044ea610b82ddc35b30
Ezequiel Ledesma <ezequiel.ledesma026@gmail.com>
2021-09-26 13:00:29 -0300

    se agrega otra ejemplo

--- archivos_base_buenas_practicas_java_script/3 - Variables, estructuras y objetos/3.3 -  palabras_reservadas/app.js
+++ archivos_base_buenas_practicas_java_script/3 - Variables, estructuras y objetos/3.3 -  palabras_reservadas/app.js
@@ -1,5 +1,11 @@
 function myCatch(ball){
 	console.log(ball)
 }
+//agregue otra salida por pantalla para probar como sale
+function otra(tennis){
+	console.log(tennis)
+}
 
 myCatch("beisball");
+//agregue otra salida por pantalla para probar como sale
+otra("tennis");
