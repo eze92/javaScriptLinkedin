f2122e7da14d6c6ccc7401299ad45161a9dff715
Ezequiel Ledesma <ezequiel.ledesma026@gmail.com>
2021-09-26 13:14:49 -0300

    se agregan comentairios

--- archivos_base_buenas_practicas_java_script/3 - Variables, estructuras y objetos/3.4 - calculos_innecesarios/app.js
+++ archivos_base_buenas_practicas_java_script/3 - Variables, estructuras y objetos/3.4 - calculos_innecesarios/app.js
@@ -2,6 +2,12 @@ var animales = ["perro", "gato", "pez"];
 
 var totalAnimales = animales.length;
 
+/*el ejemplo original tenia el el ciclo for que i < animales.length
+al hacer esto ocasiona que se vuelva ejecutar otra vez la iteracion
+para saber el largo , por lo cual es mucho mas eficiente agregar el largo
+en una variable y asi se va ejecutar una sola vez en vez de cada iteracion
+del for */
+
 for (var i = 0; i < totalAnimales; i++) {
-	console.log(animales[i]);
+	console.log(animales[i]); // si yo no le agrego la posicion del arreglo pop pantala me mostrara el arreglo completo
 };
