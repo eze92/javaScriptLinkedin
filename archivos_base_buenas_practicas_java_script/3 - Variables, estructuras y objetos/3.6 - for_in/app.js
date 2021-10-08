be117b38d96b7526eb1be11af5383a579b0e11cf
Ezequiel Ledesma <ezequiel.ledesma026@gmail.com>
2021-10-08 18:14:55 -0300

    se suben comentarios

--- archivos_base_buenas_practicas_java_script/3 - Variables, estructuras y objetos/3.6 - for_in/app.js
+++ archivos_base_buenas_practicas_java_script/3 - Variables, estructuras y objetos/3.6 - for_in/app.js
@@ -8,7 +8,10 @@ var persona = {
 }
 
 var dato = "";
-
+/*utilizo el for in para iterar sobre las propiedades del objeto persona 
+ya sea para ver si existe o para ver los valores almacenados
+si utilizo consolo.log(dato,persona[dato]) accedo a lo que tiene almacenado
+*/
 for (dato in persona){
 	console.log(dato, persona[dato]);
 }
