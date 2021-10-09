0aaec1110fe708fb1ee09de76e12f7ff24484bd8
Ezequiel Ledesma <ezequiel.ledesma026@gmail.com>
2021-10-09 15:22:52 -0300

    prueba de performance

--- archivos_base_buenas_practicas_java_script/6 - Performance/6.3 - rendimiento/app.js
+++ archivos_base_buenas_practicas_java_script/6 - Performance/6.3 - rendimiento/app.js
@@ -6,5 +6,6 @@ var l = animales.length;
 for (var i = 0; i < 10000; i++) {
     animales[i] = i;
 }
-
+//uso para ver cuanto tiempo toma la consulta debo  agregarlo el inicio y al final 
+// en todo el codigo o en una porcion 
 console.timeEnd('revisión')
