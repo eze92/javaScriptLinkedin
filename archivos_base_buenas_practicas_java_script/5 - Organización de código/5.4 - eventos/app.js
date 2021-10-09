492271e4ac0d7e2fd84d3184c4f61babdd1b0430
Ezequiel Ledesma <ezequiel.ledesma026@gmail.com>
2021-10-09 14:45:34 -0300

    forma de integrar un evento como una funcion

--- archivos_base_buenas_practicas_java_script/5 - Organización de código/5.4 - eventos/app.js
+++ archivos_base_buenas_practicas_java_script/5 - Organización de código/5.4 - eventos/app.js
@@ -1,5 +1,6 @@
 var btnSaludar = document.getElementById("btnSaludar");
 
+//forma de integrar un evento como una funcion
 btnSaludar.addEventListener('click', function(){
 	console.log("¡Hola Mundo!");
 })
