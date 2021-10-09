b9b555dc942afc4e52b52dd8487ade1d9a190436
Ezequiel Ledesma <ezequiel.ledesma026@gmail.com>
2021-10-09 14:41:08 -0300

    se agregan detalles

--- archivos_base_buenas_practicas_java_script/5 - Organización de código/5.3 - duplicidad/app.js
+++ archivos_base_buenas_practicas_java_script/5 - Organización de código/5.3 - duplicidad/app.js
@@ -5,14 +5,14 @@ var cuentaNueva = 20 + 45;
 var totalFinal = cuenta + cuentaNueva;
 
 console.log(totalFinal);
-
+//creo una funcion para sumar y luego reutilizar
 function suma(datoA, datoB){
 	var resultado = datoA + datoB;
 	return resultado;
 }
 
 suma(2,3);
-
+// encapsulo todo la operacion para evitar la duplicidad 
 var cuentaFinal = suma(suma(2, 3), suma(20, 45));
 
 //suma(5, 65)
--- archivos_base_buenas_practicas_java_script/5 - Organización de código/5.5 - bloques/app.js
+++ archivos_base_buenas_practicas_java_script/5 - Organización de código/5.5 - bloques/app.js
@@ -4,6 +4,8 @@ if(estado)
 	console.log("avanzar");
 	var resultado = 2+2;
 
+
+	//lo de arriba es lo mismo que escribir de la siguiente manera
 if(estado){
 	console.log("avanzar");
 }
