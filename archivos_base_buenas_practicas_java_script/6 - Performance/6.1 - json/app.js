c76f15675c01c4b89be9c2d43745d8804324b7b9
Ezequiel Ledesma <ezequiel.ledesma026@gmail.com>
2021-10-09 15:10:22 -0300

    comentarios sobre json

--- archivos_base_buenas_practicas_java_script/6 - Performance/6.1 - json/app.js
+++ archivos_base_buenas_practicas_java_script/6 - Performance/6.1 - json/app.js
@@ -1,5 +1,10 @@
+//esto toma los datos del archivos json
 //$.getJSON("productos.json", function(productos){
+
+//datos tomados de un archivo json remoto
 var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Santo%20Domingo%2C%20DO%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+
+//comando de jquery
 $.getJSON(url, function(productos){
 	console.log(productos);
 /*
